8c4f18b603f9d43c18f52a055199cebc74745f88
Jean Leonço <jeang.leonco@gmail.com>
2019-11-29 00:28:14 -0300

    chore(students): tests for query params and invalid student id

--- packages/api/test/functional/student.spec.js
+++ packages/api/test/functional/student.spec.js
@@ -1,27 +1,40 @@
 const { test, trait, before } = use('Test/Suite')('Student');
 const Factory = use('Factory');
+const User = use('App/Models/User');
 
 trait('Test/ApiClient');
+trait('Auth/Client');
 
 before(async () => {
   await Factory.model('App/Models/Student').createMany(5);
 });
 
 test('should be able to fetch students', async ({ client }) => {
-  const { body } = await client
-    .post('sessions')
-    .send({
-      email: 'admin@gympoint.com',
-      password: '123456',
-    })
-    .end();
+  const user = await User.find(1);
 
   const response = await client
     .get('students')
-    .header('Authorization', `Bearer ${body.token}`)
+    .loginVia(user)
     .end();
 
   response.assertStatus(200);
+});
+
+test('should be able to fetch students with name query', async ({ client }) => {
+  const user = await User.find(1);
+
+  const student = await Factory.model('App/Models/Student').create();
+
+  const response = await client
+    .get('students')
+    .query({ name: student.name })
+    .loginVia(user)
+    .end();
+
+  response.assertJSONSubset({
+    data: [{ name: student.name }],
+    total: 1,
+  });
 });
 
 test('should not be able to fetch students without jwt', async ({ client }) => {
@@ -31,20 +44,14 @@ test('should not be able to fetch students without jwt', async ({ client }) => {
 });
 
 test('should be able to create student', async ({ client }) => {
-  const { body } = await client
-    .post('sessions')
-    .send({
-      email: 'admin@gympoint.com',
-      password: '123456',
-    })
-    .end();
+  const user = await User.find(1);
 
   const student = await Factory.model('App/Models/Student').make();
 
   const response = await client
     .post('students')
     .send(student.toJSON())
-    .header('Authorization', `Bearer ${body.token}`)
+    .loginVia(user)
     .end();
 
   response.assertStatus(200);
@@ -60,20 +67,27 @@ test('should not be able to create student without jwt', async ({ client }) => {
 });
 
 test('should be able to show student', async ({ client }) => {
-  const { body } = await client
-    .post('sessions')
-    .send({
-      email: 'admin@gympoint.com',
-      password: '123456',
-    })
-    .end();
+  const user = await User.find(1);
 
   const response = await client
     .get('students/1')
-    .header('Authorization', `Bearer ${body.token}`)
+    .loginVia(user)
     .end();
 
   response.assertStatus(200);
+});
+
+test('should not be able to show student with invalid id', async ({
+  client,
+}) => {
+  const user = await User.find(1);
+
+  const response = await client
+    .get('students/4000')
+    .loginVia(user)
+    .end();
+
+  response.assertStatus(404);
 });
 
 test('should not be able to show student without jwt', async ({ client }) => {
@@ -83,23 +97,33 @@ test('should not be able to show student without jwt', async ({ client }) => {
 });
 
 test('should be able to update student', async ({ client }) => {
-  const { body } = await client
-    .post('sessions')
-    .send({
-      email: 'admin@gympoint.com',
-      password: '123456',
-    })
-    .end();
+  const user = await User.find(1);
 
   const student = await Factory.model('App/Models/Student').make();
 
   const response = await client
     .put('students/1')
-    .header('Authorization', `Bearer ${body.token}`)
+    .loginVia(user)
     .send(student.toJSON())
     .end();
 
   response.assertStatus(200);
+});
+
+test('should not be able to update student with invalid id', async ({
+  client,
+}) => {
+  const user = await User.find(1);
+
+  const student = await Factory.model('App/Models/Student').make();
+
+  const response = await client
+    .put('students/4000')
+    .loginVia(user)
+    .send(student.toJSON())
+    .end();
+
+  response.assertStatus(404);
 });
 
 test('should not be able to update student without jwt', async ({ client }) => {
@@ -112,20 +136,27 @@ test('should not be able to update student without jwt', async ({ client }) => {
 });
 
 test('should be able to delete student', async ({ client }) => {
-  const { body } = await client
-    .post('sessions')
-    .send({
-      email: 'admin@gympoint.com',
-      password: '123456',
-    })
-    .end();
+  const user = await User.find(1);
 
   const response = await client
     .delete('students/2')
-    .header('Authorization', `Bearer ${body.token}`)
+    .loginVia(user)
     .end();
 
   response.assertStatus(200);
+});
+
+test('should not be able to delete student with invalid id', async ({
+  client,
+}) => {
+  const user = await User.find(1);
+
+  const response = await client
+    .delete('students/4000')
+    .loginVia(user)
+    .end();
+
+  response.assertStatus(404);
 });
 
 test('should not be able to delete student without jwt', async ({ client }) => {
