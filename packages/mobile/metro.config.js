cacda05f63c800782655057b1273ce58d994f490
Jean Leonço <jeang.leonco@gmail.com>
2019-12-01 19:21:25 -0300

    feature(lerna): monorepo and lerna configuration

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -5,7 +5,43 @@
  * @format
  */
 
+const path = require('path');
+
+const getWorkspaces = require('get-yarn-workspaces'); //eslint-disable-line
+const blacklist = require('metro-config/src/defaults/blacklist'); //eslint-disable-line
+
+const workspaces = getWorkspaces(__dirname);
+
 module.exports = {
+  projectRoot: path.resolve(__dirname, '.'),
+
+  watchFolders: [path.resolve(__dirname, '../../node_modules')],
+
+  resolver: {
+    blacklistRE: blacklist(
+      workspaces
+        .filter(
+          workspacePath => workspacePath.indexOf('packages/mobile') === -1
+        )
+        .map(
+          workspacePath =>
+            new RegExp(
+              `^${escape(
+                path.resolve(__dirname, workspacePath, 'node_modules')
+              )}\\/.*$`
+            )
+        )
+    ),
+    // https://github.com/facebook/metro/issues/1#issuecomment-453450709
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
+      }
+    ),
+  },
+
+  // http://facebook.github.io/react-native/blog/2019/03/12/releasing-react-native-059#faster-app-launches-with-inline-requires
   transformer: {
     getTransformOptions: async () => ({
       transform: {
